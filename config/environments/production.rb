648798cb69f78d5ae2ea1f7a031d9e4f3f4f43a1
Vladimir Mozgin <mozgin.vova@gotoinc.co>
2020-12-06 19:19:23 +0200

    Fix CORS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:8080', 'https://dmytro-havrysh-frontend.herokuapp.com', 'http://dmytro-havrysh-frontend.herokuapp.com']
-  config.web_socket_server_url = "wss://dmytro-havrysh-frontend.herokuapp.com/cable" 
+  config.web_socket_server_url = "wss://dmytro-havrysh-frontend.herokuapp.com/cable"
+  Rails.application.config.hosts << "dmytro-havrysh-frontend.herokuapp.com"
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
