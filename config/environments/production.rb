547f5ff6cdd7d7c9562e213adfe48e72588492e8
Vladimir Mozgin <mozgin.vova@gotoinc.co>
2020-12-06 18:20:14 +0200

    Fix deplot config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:8080']
+  Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:8080', 'https://dmytro-havrysh-frontend.herokuapp.com/']
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
